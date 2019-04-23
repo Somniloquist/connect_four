d8a1d00251af9ba333f7426c153b7d4390831c4b
Robin Unger <robin.e.unger@gmail.com>
2019-04-23 14:14:37 -0500

    add player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,2 +1,8 @@
 class Player
+  @@count = 1
+  attr_reader :name
+  def initialize(name = "player#{@@count}")
+    @@count += 1
+    @name = name
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,4 +1,17 @@
 require "player.rb"
 
 describe Player do
+  describe "#initialize" do
+    it "initializes with default player names if no name is given" do
+      player1 = Player.new
+      player2 = Player.new
+      expect(player1.name).to eql("player1")
+      expect(player2.name).to eql("player2")
+    end
+
+    it "accepts a name as an argument" do
+      player1 = Player.new("robin")
+      expect(player1.name).to eql("robin")
+    end
+  end
 end
